5911e4c16057673f84fe35b0d6cfd975b4267495
gness1804 <graham.nessler@gmail.com>
2016-12-07 21:51:33 -0700

    onClick changes state in input

--- lib/components/Input.jsx
+++ lib/components/Input.jsx
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class Input extends Component {
 
   render() {
+
     return (
-      <div>Hello! I am the input component.</div>
+      <div id="input-items-container">
+        <input id="item-input" ref={(c) => { this.name = c; }} type="text" placeholder="Item Name" list="groceries" />
+        <input id="aisle-input" ref={(c) => { this.aisle = c; }} type="text" placeholder="Aisle" />
+        <input id="quantity-input" ref={(c) => { this.quantity = c; }} type="text" placeholder="Quantity" />
+        <input id="note-input" ref={(c) => { this.note = c; }} type="text" placeholder="Note" />
+        <button onClick={(e) => {this.props.addItem('foo bread', 9, 'foo', 'foo')} }>Submit</button>
+      </div>
     );
   }
 
--- lib/containers/InputContainer.jsx
+++ lib/containers/InputContainer.jsx
@@ -16,4 +16,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Input)
+export default connect(mapStateToProps, mapDispatchToProps)(Input);
