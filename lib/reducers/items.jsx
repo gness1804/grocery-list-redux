e04bc86994d4243f84e05fd7208c492c8f127abe
gness1804 <graham.nessler@gmail.com>
2016-12-14 12:39:20 -0700

    reducers all set items to local storage

--- lib/reducers/items.jsx
+++ lib/reducers/items.jsx
@@ -7,20 +7,32 @@ const items = (state = [], action) => {
       localStorage.setItem('items', JSON.stringify(state.concat({ name: action.name, aisle: action.aisle, quantity: action.quantity, note: action.note, id: Date.now() })));
       return state.concat({ name: action.name, aisle: action.aisle, quantity: action.quantity, note:action.note, id: Date.now() });
     case 'DELETE_ITEM':
+    localStorage.setItem('items', JSON.stringify(state.filter((i) => { return i.id !== action.id })));
       return state.filter((i) => { return i.id !== action.id });
     case 'DELETE_ALL_ITEMS':
+      localStorage.setItem('items', JSON.stringify([]));
       return [];
     case 'SORT_AISLE':
       state.forEach((item) => {
         arr.push(item);
       });
+      localStorage.setItem('items', JSON.stringify(arr.sort((a, b) => { return a.aisle - b.aisle })));
       return arr.sort((a, b) => { return a.aisle - b.aisle });
     case 'SORT_ALPHA':
       state.forEach((item) => {
         arr.push(item);
       });
+      localStorage.setItem('items', JSON.stringify(_.sortBy(arr, 'name')));
       return _.sortBy(arr, 'name');
     case 'EDIT_ITEM':
+    localStorage.setItem('items', JSON.stringify(state.map(item => {
+      if (item.id !== action.id) {
+        return item;
+      }
+      return Object.assign({}, item, {
+        name: action.name, aisle: action.aisle, quantity: action.quantity, note: action.note, id: action.id,
+      });
+    })));
       return state.map(item => {
         if (item.id !== action.id) {
           return item;
