e2f0a8a96b631e70b81f4372001ac59624cda92d
gness1804 <graham.nessler@gmail.com>
2016-12-14 16:37:00 -0700

    adds styles from old project

--- lib/reducers/items.jsx
+++ lib/reducers/items.jsx
@@ -7,13 +7,16 @@ const items = (state = [], action) => {
       localStorage.setItem('items', JSON.stringify(state.concat({ name: action.name, aisle: action.aisle, quantity: action.quantity, note: action.note, id: Date.now() })));
       return state.concat({ name: action.name, aisle: action.aisle, quantity: action.quantity, note: action.note, id: Date.now() });
     case 'DELETE_ITEM':
-    localStorage.setItem('items', JSON.stringify(state.filter((i) => { return i.id !== action.id })));
-      return state.filter((i) => { return i.id !== action.id });
-    case 'DELETE_ALL_ITEMS':
-      const warning = confirm('This will delete ALL items! Are you sure?');
+      const warning = confirm('Are you sure you want to delete this item?');
       if (warning) {
+        localStorage.setItem('items', JSON.stringify(state.filter((i) => { return i.id !== action.id })));
+        return state.filter((i) => { return i.id !== action.id });
+      }
+    case 'DELETE_ALL_ITEMS':
+      const warning2 = confirm('This will delete ALL items! Are you sure?');
+      if (warning2) {
         localStorage.setItem('items', JSON.stringify([]));
-        return [];  
+        return [];
       }
     case 'SORT_AISLE':
       state.forEach((item) => {
