751459e3152d3c0fa60d843d6ca38bdcf3946d95
gness1804 <graham.nessler@gmail.com>
2016-12-14 19:21:53 -0700

    add submit button disable functionality

--- lib/components/Input.jsx
+++ lib/components/Input.jsx
@@ -5,6 +5,12 @@ import Datalist from './Datalist';
 import aisles from './Aisles';
 
 class Input extends Component {
+  constructor() {
+    super();
+    this.state = {
+      submitDisabled: true,
+    };
+  }
 
   componentDidMount() {
     const items = JSON.parse(localStorage.getItem('items'));
@@ -19,6 +25,29 @@ class Input extends Component {
     }
   }
 
+  // checkInput() {
+  //   if (document.getElementById('item-input').value !== '') {
+  //     this.setState({ submitDisabled: false });
+  //   } else {
+  //     this.setState({ submitDisabled: true });
+  //   }
+  // }
+
+  checkInputState(e) {
+  if (e) {
+    if (e.target.value !== '') {
+      this.setState({ submitDisabled: false });
+    } else {
+      this.setState({ submitDisabled: true });
+    }
+  }
+  if (document.getElementById('item-input').value !== '') {
+    this.setState({ submitDisabled: false });
+  } else {
+    this.setState({ submitDisabled: true });
+  }
+}
+
   submitItem() {
     this.props.addItem(this.name.value, this.aisle.value, this.quantity.value, this.note.value);
     document.getElementById('item-input').value = '';
@@ -26,6 +55,7 @@ class Input extends Component {
     document.getElementById('quantity-input').value = '';
     document.getElementById('note-input').value = '';
     document.getElementById('category').value = 'Please choose a category.';
+    this.setState({ submitDisabled: true });
   }
 
   render() {
@@ -34,13 +64,13 @@ class Input extends Component {
 
     return (
       <div id="input-items-container">
-        <input id="item-input" ref={(c) => { this.name = c; }} type="text" placeholder="Item Name" list="groceries" defaultValue="" />
+        <input id="item-input" ref={(c) => { this.name = c; }} type="text" placeholder="Item Name" list="groceries" defaultValue="" onChange={(e) => { this.checkInputState(e)} } onBlur={() => { this.checkInputState()} }/>
         <Category assignAisle={this.assignAisle} />
         <Datalist />
         <input id="aisle-input" ref={(c) => { this.aisle = c; }} type="text" placeholder="Aisle" />
         <input id="quantity-input" ref={(c) => { this.quantity = c; }} type="text" placeholder="Quantity" />
         <input id="note-input" ref={(c) => { this.note = c; }} type="text" placeholder="Note" />
-        <button onClick={() => { this.submitItem() }} id="submit-button">Submit</button>
+        <button onClick={() => { this.submitItem() }} id="submit-button" disabled={this.state.submitDisabled}>Submit</button>
         <button onClick={() => { sortAisle() }} id="sort-items-button" type="button">Sort by Aisle</button>
         <button onClick={() => { sortAlpha() }} id="sort-alpha-button" type="button">Sort Alpha</button>
         <button id="delete-all-items-button" type="button" onClick={() => { deleteAllItems() }}>Delete ALL Items!</button>
